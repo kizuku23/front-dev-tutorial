cfe82e90fd3488438b994910ddf662eb2e717bcc
vagrant <km.dwc23@gmail.com>
2020-12-21 14:02:52 +0000

    [Finish] 4-30-2

---
+++ 04_JavaScriptの基礎を固める/030_メソッドとプロパティー/start2/main.js
@@ -0,0 +1,14 @@
+// let hello = 'hello world';
+// console.log(hello.length); プロパティ: 特定の値を保持しているもの
+
+// let hello = 'hello world';
+// console.log(hello.toUpperCase());　メソッド:　特定の処理を行うためのもの
+
+// let hello = 'hello world';
+// console.log(hello.substring(0, 5));
+
+// let hello = 'hello world';
+// console.log(hello.slice(0, 5));
+
+let hello = 'hello world';
+console.log(hello.split(' '));
