d3f3bf6d5016ec7775a23ec66d0449282ac8c821
vagrant <km.dwc23@gmail.com>
2020-12-22 06:56:40 +0000

    [Finish] 4-60-2

---
+++ 04_JavaScriptの基礎を固める/060_ループの記述方法/start2/main.js
@@ -0,0 +1,11 @@
+const arry = [1,2,3,4,5];
+
+// for(初期化; 条件; 式) {}
+for(let i = 0; i < 5; i = i + 1) {
+  console.log(arry[i]);
+}
+
+const arry2 = [1,2,3,4,5,6];
+for(let i = 0; i < arry2.length; i++) {
+  console.log(arry2[i]);
+}
