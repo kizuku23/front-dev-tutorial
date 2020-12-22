4124117745f2c2adada2f1159c98443f68714a36
vagrant <km.dwc23@gmail.com>
2020-12-22 13:26:51 +0000

    [Finish] 4-70-2

---
+++ 04_JavaScriptの基礎を固める/070_関数定義とアロー関数/start2/main.js
@@ -0,0 +1,23 @@
+// function hello(name = 'Tom') {
+//   console.log('hello' + name);
+// }
+
+// const hello = function(name = 'Tom') {
+//   console.log('hello' + name);
+// }
+
+// const hello = (name = 'Tom') => console.log('hello' + name);
+// const hello = (name, age) => console.log('hello' + name + age);
+// const hello = name => console.log('hello' + name);
+// const hello = (name, age) => 40
+
+const arry = [1,2,3,4,5,6];
+
+// arry.forEach(function(value) {
+//   console.log(value);
+// }) 
+
+arry.forEach(value => console.log(value));
+
+// hello('Code Mafia', 10);
+// hello('Code Mafia2', 20);
